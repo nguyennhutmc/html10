0fc9c052a5b01d633cb87343568310858f49016c
Nhut Nguyen <nhutnguyen@vba-adk.com>
2025-04-18 16:46:30 +0700

    up

---
+++ page2.php
@@ -0,0 +1,91 @@
+<?php include 'header.php'; ?>
+    <main class="home_page">
+        <div class="page2">
+            <section class="section1 style2">
+                <div class="container">
+                    <div class="row g-0">
+                        <div class="col-lg-auto col_left">
+                            <div class="col_inner">
+                                <div class="banner_main">
+                                <div class="swiper">
+                                    <div class="swiper-wrapper">
+                                        <div class="swiper-slide"><img class="w-100" src="images/banner2.png" alt=""></div>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg col_right">
+                            <div class="col_inner">
+                            <h2 class="txt1">PSYCHOTHERAPIE</h2>
+                            <h1 class="txt2">Heilung und
+                                innere Stärke
+                                durch
+                                Verbundenheit
+                                mit sich selbst</h1>
+                            </div>
+                        </div>
+                        <div class="col-lg-12 col_bottom">
+                            <div class="col_inner">
+                                <h2 class="txt3">WARUM KÖRPERPSYCHOTHERAPIE?</h2>
+                                <div class="text text-editor">
+                                    <p>In der ganzheitlichen Körperpsychotherapie stehen Sie als Mensch mit Körper, Geist und
+                                        Seele im Mittelpunkt. Belastende Erfahrungen hinterlassen oft Spuren, die Ihr Denken,
+                                        Fühlen und Erleben beeinflussen. Gemeinsam erkunden wir, wie Sie diese Spuren verstehen
+                                        und integrieren können, um wieder mehr Sicherheit und Verbundenheit zu spüren. Als
+                                        Heilpraktikerin für Psychotherapie unterstütze ich Sie dabei, Ihre Selbstregulation zu stärken
+                                        und bewusster mit Ihrer inneren Dynamik umzugehen.</p>
+                                </div>
+                                <h3 class="txt4">Warum traumasensibel und bindungsorientiert?</h3>
+                                <div class="text text-editor">
+                                    <p>Stress und belastende Erfahrungen können das Gefühl von Sicherheit in uns und unseren
+                                        Beziehungen beeinträchtigen. Mit meinem traumasensiblen, bindungsorientierten Ansatz
+                                        begleite ich Sie behutsam – ohne Druck oder Überforderung. In diesem geschützten Raum
+                                        können Sie lernen, Blockaden zu lösen und sich sicherer in sich selbst zu fühlen. So entsteht
+                                        nachhaltige Heilung, getragen von innerer Stärke und neuen Bindungserfahrungen.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="section4 style2">
+                <div class="container">
+                    <h2 class="txt1 fw-normal">Alles, was du fühlst, ergibt Sinn.<sub class="ms-2 fw-semibold">(VERENA KÖNIG)</sub></h2>
+                    <div class="list">
+                        <div class="row g-x">
+                            <div class="col-lg-6">
+                                <div class="item vstack position-relative">
+                                    <div class="icon position-absolute"><img src="images/section4_icon_thumb_01.png" alt="" class="img w-100"></div>
+                                    <div class="txt2">PODCAST</div>
+                                    <div class="txt3">„Einfach Stark"</div>
+                                    <div class="text text-editor">
+                                        <p>Der Podcast mit Kaj-Ariane Fischer voller Inspiration für alle, die Ihr Leben so gestalten wollen, dass es sie erfüllt. Mit konkreten Tipps und Beispielen aus der Praxis wird alle zwei Wochen hinter die Kulissen des Lebens geschaut, um wirklich voranzukommen.</p>
+                                        <p>Der Podcast inspiriert zum Nachdenken, bringt neue Ideen und macht Lust darauf, Neues ausprobieren und Veränderungen aktiv anzugehen.</p>
+                                    </div>
+                                    <div class="text-start mt-auto">
+                                        <a href="#" class="btn btn-primary">Zum Podcast</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="item vstack position-relative">
+                                    <div class="icon position-absolute"><img src="images/section4_icon_thumb_02.png" alt="" class="img w-100"></div>
+                                    <div class="txt2">BLOG</div>
+                                    <div class="txt3">Thema Blogbeitrag</div>
+                                    <div class="text text-editor">
+                                        <p>Der Podcast mit Kaj-Ariane Fischer voller Inspiration für alle, die Ihr Leben so gestalten wollen, dass es sie erfüllt. Mit konkreten Tipps und Beispielen aus der Praxis wird alle zwei Wochen hinter die Kulissen des Lebens geschaut, um wirklich voranzukommen.</p>
+                                        <p>Der Podcast inspiriert zum Nachdenken, bringt neue Ideen und macht Lust darauf, Neues ausprobieren und Veränderungen aktiv anzugehen.</p>
+                                    </div>
+                                    <div class="text-start mt-auto">
+                                        <a href="#" class="btn btn-primary">Zum Beitrag</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </main>
+<?php include 'footer.php'; ?>
