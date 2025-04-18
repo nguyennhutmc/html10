c20b723ccdba62183e0e330367c4f03605423b20
Nhut Nguyen <nhutnguyen@vba-adk.com>
2025-04-18 15:25:20 +0700

    up

--- js/script.js
+++ js/script.js
@@ -1,16 +1,39 @@
 var vw = ($('html').css('font-size').replace('px', ''));
+
 //console.log(vw);
+function init() {
+    if ($(window).width() >= 992) {
+        let hideTimeout;
+        $('.nav-item.dropdown').hover(
+            function () {
+                clearTimeout(hideTimeout);
+                $(this).addClass('show');
+                $(this).find('.dropdown-toggle').addClass('show');
+                $(this).find('.dropdown-menu').addClass('show');
+            },
+            function () {
+                const $dropdown = $(this);
+                hideTimeout = setTimeout(function () {
+                    $dropdown.removeClass('show');
+                    $dropdown.find('.dropdown-toggle').removeClass('show');
+                    $dropdown.find('.dropdown-menu').removeClass('show');
+                }, 300);
+            }
+        );
+    }
+}
 
 function _sticky() {
     var winscroll = $(window).scrollTop();
     if (winscroll >= vw * 10) {
         $("body").addClass("sticky");
-    }
-    else{
+    } else {
         $("body").removeClass("sticky");
     }
 }
+
 $(function () {
+    init();
 });
 var lastScrollTop = 0;
 $(window).on("scroll", function (event) {
