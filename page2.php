aceb41835a745ad38e367c712c89fe9c7557c171
nguyennhutmc <nguyen.nhutmc@gmail.com>
2025-04-19 19:02:16 +0700

    up

--- page2.php
+++ page2.php
@@ -1,93 +1,121 @@
 <?php include 'header.php'; ?>
-    <main class="home_page">
-        <div class="page2">
-            <section class="section1 style2">
-                <div class="container">
-                    <div class="row g-0">
-                        <div class="col-lg-auto col_left">
-                            <div class="col_inner">
-                                <div class="banner_main">
-                                <div class="swiper">
-                                    <div class="swiper-wrapper">
-                                        <div class="swiper-slide"><img class="w-100" src="images/banner2.png" alt=""></div>
-                                    </div>
-                                </div>
-                                </div>
-                            </div>
+<main class="home_page">
+   <div class="page2">
+      <section class="section1 style2">
+         <div class="container">
+            <div class="row g-0">
+               <div class="col-lg-auto col_left">
+                  <div class="col_inner">
+                     <div class="banner_main">
+                        <div class="swiper">
+                           <div class="swiper-wrapper">
+                              <div class="swiper-slide"><img class="w-100" src="images/banner2.png" alt=""></div>
+                           </div>
                         </div>
-                        <div class="col-lg col_right">
-                            <div class="col_inner">
-                            <h2 class="txt1">PSYCHOTHERAPIE</h2>
-                            <h1 class="txt2">Heilung und
-                                innere Stärke
-                                durch
-                                Verbundenheit
-                                mit sich selbst</h1>
-                            </div>
-                        </div>
-                        <div class="col-lg-12 col_bottom">
-                            <div class="col_inner">
-                                <h2 class="txt3">WARUM KÖRPERPSYCHOTHERAPIE?</h2>
-                                <div class="text text-editor">
-                                    <p>In der ganzheitlichen Körperpsychotherapie stehen Sie als Mensch mit Körper, Geist und
-                                        Seele im Mittelpunkt. Belastende Erfahrungen hinterlassen oft Spuren, die Ihr Denken,
-                                        Fühlen und Erleben beeinflussen. Gemeinsam erkunden wir, wie Sie diese Spuren verstehen
-                                        und integrieren können, um wieder mehr Sicherheit und Verbundenheit zu spüren. Als
-                                        Heilpraktikerin für Psychotherapie unterstütze ich Sie dabei, Ihre Selbstregulation zu stärken
-                                        und bewusster mit Ihrer inneren Dynamik umzugehen.</p>
-                                        <p>&nbsp;</p>
-                                </div>
-                                <h3 class="txt4">Warum traumasensibel und bindungsorientiert?</h3>
-                                <div class="text text-editor">
-                                    <p>Stress und belastende Erfahrungen können das Gefühl von Sicherheit in uns und unseren
-                                        Beziehungen beeinträchtigen. Mit meinem traumasensiblen, bindungsorientierten Ansatz
-                                        begleite ich Sie behutsam – ohne Druck oder Überforderung. In diesem geschützten Raum
-                                        können Sie lernen, Blockaden zu lösen und sich sicherer in sich selbst zu fühlen. So entsteht
-                                        nachhaltige Heilung, getragen von innerer Stärke und neuen Bindungserfahrungen.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <div class="container">
+                     </div>
+                  </div>
+               </div>
+               <div class="col-lg col_right">
+                  <div class="col_inner">
+                     <h2 class="txt1">PSYCHOTHERAPIE</h2>
+                     <h1 class="txt2">Heilung und
+                        innere Stärke
+                        durch
+                        Verbundenheit
+                        mit sich selbst
+                     </h1>
+                  </div>
+               </div>
+               <div class="col-lg-12 col_bottom">
+                  <div class="col_inner">
+                     <h2 class="txt3">WARUM KÖRPERPSYCHOTHERAPIE?</h2>
+                     <div class="text text-editor">
+                        <p>In der ganzheitlichen Körperpsychotherapie stehen Sie als Mensch mit Körper, Geist und
+                           Seele im Mittelpunkt. Belastende Erfahrungen hinterlassen oft Spuren, die Ihr Denken,
+                           Fühlen und Erleben beeinflussen. Gemeinsam erkunden wir, wie Sie diese Spuren verstehen
+                           und integrieren können, um wieder mehr Sicherheit und Verbundenheit zu spüren. Als
+                           Heilpraktikerin für Psychotherapie unterstütze ich Sie dabei, Ihre Selbstregulation zu stärken
+                           und bewusster mit Ihrer inneren Dynamik umzugehen.
+                        </p>
+                        <p>&nbsp;</p>
+                     </div>
+                     <h3 class="txt4">Warum traumasensibel und bindungsorientiert?</h3>
+                     <div class="text text-editor">
+                        <p>Stress und belastende Erfahrungen können das Gefühl von Sicherheit in uns und unseren
+                           Beziehungen beeinträchtigen. Mit meinem traumasensiblen, bindungsorientierten Ansatz
+                           begleite ich Sie behutsam – ohne Druck oder Überforderung. In diesem geschützten Raum
+                           können Sie lernen, Blockaden zu lösen und sich sicherer in sich selbst zu fühlen. So entsteht
+                           nachhaltige Heilung, getragen von innerer Stärke und neuen Bindungserfahrungen.
+                        </p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <section class="section2 style2">
+         <div class="container">
             <h2 class="fw-normal title_big">Alles, was du fühlst, ergibt Sinn.<sub class="ms-2 fw-semibold">(VERENA KÖNIG)</sub></h2>
+            <div class="list vstack">
+               <div class="item">
+                  <div class="image">
+                     <img src="images/thumb_01_new.png" alt="" class="img w-100">
+                  </div>
+                  <div class="box_text vstack">
+                     <h3 class="txt1">MEIN ANSATZ - WAS IST BEI MIR ANDERS?</h3>
+                     <div class="text">In meiner Arbeit lege ich besonderen Wert auf Verbundenheit, Akzeptanz und Augenhöhe. Oft erlebe ich, dass diese Haltung allein schon Heilung in Gang setzen kann. Ich unterstütze Sie, indem ich mich immer an die Seite Ihrer gesunden Kräfte stelle. Für mich gibt es keinen „Defekt", der repariert werden muss - stattdessen geht es darum, Erfahrungen zu integrieren und mehr der Mensch zu werden, der Sie im Inneren sind.</div>
+                     <h4 class="txt4">Wie arbeiten wir?</h4>
+                     <div class="text">Wir betrachten Sie ganzheitlich. Das bedeutet, wir arbeiten sowohl „Top-Down" - mit Gedanken, Glaubenssystemen und Einsichten, als auch „Bottom-up" - mit Ihrem Körper, Ihrem Nervensystem und ihren Gefühlen. Diese Ebenen sind untrennbar miteinander verbunden und tragen gemeinsam zur Heilung bei.</div>
+                  </div>
+               </div>
             </div>
-            <section class="section2 style2">
-                <div class="container">
-                    <div class="list vstack">
-                        <div class="item">
-                            <div class="image">
-                                <img src="images/thumb_01_new.png" alt="" class="img w-100">
-                            </div>
-                            <div class="box_text vstack">
-                            
-                                <h3 class="txt1">MEIN ANSATZ - WAS IST BEI MIR ANDERS?</h3>
-                                <div class="text">In meiner Arbeit lege ich besonderen Wert auf Verbundenheit, Akzeptanz und Augenhöhe. Oft erlebe ich, dass diese Haltung allein schon Heilung in Gang setzen kann. Ich unterstütze Sie, indem ich mich immer an die Seite Ihrer gesunden Kräfte stelle. Für mich gibt es keinen „Defekt", der repariert werden muss - stattdessen geht es darum, Erfahrungen zu integrieren und mehr der Mensch zu werden, der Sie im Inneren sind.</div>
-                                <h4 class="txt4">Wie arbeiten wir?</h4>
-                                <div class="text">Wir betrachten Sie ganzheitlich. Das bedeutet, wir arbeiten sowohl „Top-Down" - mit Gedanken, Glaubenssystemen und Einsichten, als auch „Bottom-up" - mit Ihrem Körper, Ihrem Nervensystem und ihren Gefühlen. Diese Ebenen sind untrennbar miteinander verbunden und tragen gemeinsam zur Heilung bei.</div>
-                                
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <div class="container">
-            <h2 class="fw-normal title_big">Nutzen Sie die Chance für einen Neuanfang.</h2>
+         </div>
+      </section>
+      <section class="section3 style3">
+         <div class="container">
+            <div class="col_inner">
+               <h3 class="txt1">FÜR WEN IST DIESE THERAPIE GEEIGNET?</h3>
+               <div class="text text-editor mb-0">
+                  <p><strong>Ganzheitliche Körperpsychotherapie kann Ihnen helfen, wenn Sie:</strong></p>
+                  <ul>
+                     <li>das Gefühl haben, innerlich festzustecken, als könnten Sie nicht vorwärtskommen, egal, wie sehr Sie sich bemühen.</li>
+                     <li>angespannt, erschöpft oder antriebslos sind und sich der Tag schon nach dem Aufstehen überwältigend anfühlt.</li>
+                     <li>Symptome spüren, die Sie nicht verstehen, wie innere Unruhe, Angst, körperliche Beschwerden oder ein beklemmendes Gefühl, das sich nicht lösen lässt.</li>
+                     <li>sich wieder sicherer und wohler in Ihrem Körper fühlen möchten, um Kraft und Vertrauen in sich selbst zurückzugewinnen.</li>
+                     <li>sich unverstanden oder wenig verbunden mit sich selbst und Ihrer Umgebung erleben, als wären Sie Außenseiter in Ihrem eigenen Leben.</li>
+                     <li>sich leer oder fühlen - ohne festen Halt oder festen Boden unter den Füßen, während der Alltag immer schwerer zu bewältigen scheint.</li>
+                  </ul>
+                  <p>In Krisen oder nach belastenden Erfahrungen können Sie innere Stärke und neue Perspektiven entwickeln. Auch wenn Sie bereits andere therapeutische Wege ausprobiert haben, kann diese Herangehensweise eine neue Chance für Sie sein.</p>
+               </div>
             </div>
-            <section class="section3">
-                <div class="container">
-                    <div class="col_inner pt-0">
-                    <div class="text text-editor">
-                        <p>Entdecken Sie mehr Leichtigkeit, Freude und Zuversicht in Ihrem Leben, spüren Sie wieder Verbundenheit mit sich selbst und anderen und finden Sie Sinn und Ganzheit. Lassen Sie uns gemeinsam den ersten Schritt machen – ich lade Sie herzlich ein, sich besser kennenzulernen und diesen Weg einzuschlagen.</p>
-                        <p><strong>Der erste Schritt: Melden Sie sich für ein unverbindliches Vorgespräch. Gemeinsam schauen wir, was Sie brauchen, und entwickeln Ihren individuellen Weg.</strong></p>
-                    </div>
-                    <div class="text-start mt-auto hstack gap-x">
-                        <a href="#" class="btn btn-warning">Über mich</a>
-                    </div>
-                    </div>
+         </div>
+      </section>
+      <div class="container">
+         <h2 class="fw-normal title_big">Nutzen Sie die Chance für einen Neuanfang.</h2>
+      </div>
+      <section class="section3 style2">
+         <div class="container">
+            <div class="col_inner pt-0">
+               <div class="text text-editor">
+                  <p>Entdecken Sie mehr Leichtigkeit, Freude und Zuversicht in Ihrem Leben, spüren Sie wieder Verbundenheit mit sich selbst und anderen und finden Sie Sinn und Ganzheit. Lassen Sie uns gemeinsam den ersten Schritt machen – ich lade Sie herzlich ein, sich besser kennenzulernen und diesen Weg einzuschlagen.</p>
+                  <p><strong>Der erste Schritt: Melden Sie sich für ein unverbindliches Vorgespräch. Gemeinsam schauen wir, was Sie brauchen, und entwickeln Ihren individuellen Weg.</strong></p>
+               </div>
+               <div class="text-start mt-auto hstack gap-x mb-5">
+                  <a href="#" class="btn btn-warning">Kontakt</a>
+               </div>
+               <div class="prices-section">
+               <div class="txt2">Preise & Modalitäten (online/Präsenz)</div>
+               <p><strong>Bitte beachten Sie, dass meine Praxis eine reine Privatzahlerpraxis ist.</strong></p>
+               <p>Bitte prüfen Sie, ob Sie über eine private Zusatzversicherung verfügen, oder Ihre private Krankenkasse die Kosten für </p>
+               <p><strong>Heilpraktiker für Psychotherapie</strong> übernimmt. Eine passende Rechnung stelle ich gerne aus.</p>
+               <p>&nbsp;</p>
+               <p><strong>Einzelstunde:</strong> 80,-€/60 Min, 120,-€/90 Min.*</p>
+               <p>*Reduzierte Preise und unterschiedliche Zahlungsweisen sind nach Absprache möglich.</p>
                 </div>
-            </section>
-        </div>
-    </main>
+            <p><strong>Wenn Sie unsicher sind, ob mein Ansatz für Sie passt, melden Sie sich gerne – gemeinsam finden wir heraus, ob ich die richtige Begleitung für Sie sein kann.</strong></p>
+         </div>
+   </div>
+   </section>
+   </div>
+</main>
 <?php include 'footer.php'; ?>
